3f4da5921ad9e139690d1fb9c6496f038747b36a
peter.kovacs2 <peter.kovacs2@aimotive.com>
2024-09-19 21:55:58 +0200

    Test shader loading and compilation

--- WebGL_fractal/fractal_engine.js
+++ WebGL_fractal/fractal_engine.js
@@ -1,16 +1,40 @@
 main();
 
-function main() {
-  var vertex_code = "";
-  fetch("vertex.glsl")
-    .then((resp) => resp.text())
-    .then((text) => {
-      vertex_code = text;
-    });
-  document.getElementById("demo").innerHTML = vertex_code;
+//This should be one generic loader
+async function getCompiledShaders(gl) {
+  var vertex_code = await fetch("vertex.glsl").then((resp) => {
+    return resp.text();
+  });
+  var fragment_code = await fetch("fragment.glsl").then((resp) => {
+    return resp.text();
+  });
 
+  const vertex_shader = gl.createShader(gl.VERTEX_SHADER);
+  const fragment_shader = gl.createShader(gl.FRAGMENT_SHADER);
+
+  gl.shaderSource(vertex_shader, vertex_code);
+  gl.shaderSource(fragment_shader, fragment_code);
+
+  gl.compileShader(vertex_shader);
+  gl.compileShader(fragment_shader);
+
+  if (
+    !gl.getShaderParameter(vertex_shader, gl.COMPILE_STATUS) ||
+    !gl.getShaderParameter(fragment_shader, gl.COMPILE_STATUS)
+  ) {
+    alert("An error occured with shader compilation!");
+    return null;
+  }
+
+  return [vertex_shader, fragment_shader];
+}
+
+function main() {
   const canvas = document.querySelector("#glcanvas");
   const gl = canvas.getContext("webgl");
+
+  var vertex_shader, fragment_shader;
+  [vertex_shader, fragment_shader] = getCompiledShaders(gl);
 
   // Only continue if WebGL is available and working
   if (gl === null) {
