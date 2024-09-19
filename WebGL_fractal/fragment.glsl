e0ba91988f7a4c6ce1d842bc5594e500c9dfef21
peter.kovacs2 <peter.kovacs2@aimotive.com>
2024-09-19 22:39:40 +0200

    Check fragment shader scaling

--- WebGL_fractal/fragment.glsl
+++ WebGL_fractal/fragment.glsl
@@ -1,4 +1,16 @@
+precision highp float;
+
 void main()
 {
-    gl_FragColor = vec4(gl_FragCoord.x/640.0, gl_FragCoord.y/480.0, 0.0, 1.0);
+    float x = (gl_FragCoord.x/640.0-0.5)*4;
+    float y = (gl_FragCoord.y/480.0-0.5)*4;
+
+    if(distance(vec2(x,y)) > 1.0)
+    {
+        gl_FragColor = vec4(x, y, 0.0, 1.0);
+    }
+    else
+    {
+        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+    }
 }
