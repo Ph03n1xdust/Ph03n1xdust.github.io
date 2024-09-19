24aae143843583e05f7fde6b12a50ba35e9d3a50
peter.kovacs2 <peter.kovacs2@aimotive.com>
2024-09-19 22:46:36 +0200

    Distance -> length

--- WebGL_fractal/fragment.glsl
+++ WebGL_fractal/fragment.glsl
@@ -3,7 +3,7 @@ void main()
     float x = (gl_FragCoord.x/640.0-0.5)*4.0;
     float y = (gl_FragCoord.y/480.0-0.5)*4.0;
 
-    if(distance(vec2(x,y)) > 1.0)
+    if(length(vec2(x,y)) > 1.0)
     {
         gl_FragColor = vec4(x, y, 0.0, 1.0);
     }
