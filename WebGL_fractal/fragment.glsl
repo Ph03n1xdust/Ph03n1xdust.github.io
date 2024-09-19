5e8ba809da59964c4a3990eaaa60646e14ebaa2d
peter.kovacs2 <peter.kovacs2@aimotive.com>
2024-09-19 22:59:52 +0200

    Very basic Julia test

--- WebGL_fractal/fragment.glsl
+++ WebGL_fractal/fragment.glsl
@@ -1,16 +1,33 @@
 precision highp float;
 
+vec2 complex_mult(vec2 a, vec2 b)
+{
+    return vec2(a.x*b.x-a.y*b.y, a.x*b.y+a.y*b.x);
+}
+
 void main()
 {
+    float c_real = -0.4;
+    float c_im = 0.6;
+    float thresh = 10.0;
+
+    vec2 c = vec2(c_real, c_im);
+
     float x = (gl_FragCoord.x/640.0-0.5)*4.0;
     float y = (gl_FragCoord.y/480.0-0.5)*4.0;
 
-    if(length(vec2(x,y)) > 1.0)
+    vec2 currval = vec2(x,y);
+    
+    int i = 0;
+    for(i=0; i<256; i++)
     {
-        gl_FragColor = vec4(x, y, 0.0, 1.0);
+        if(length(currval)>thresh)
+        {
+            break;
+        }
+
+        currval = complex_mult(currval, currval)+c;
     }
-    else
-    {
-        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
-    }
+
+    gl_FragColor = vec4(i/256.0, i/256.0, i/256.0, 1.0);
 }
